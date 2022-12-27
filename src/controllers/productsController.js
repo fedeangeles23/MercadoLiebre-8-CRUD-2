5655703efcf00cfa525d4ece56f3ed767b6d59e3
Fede Angeles <fedeangeles23@gmail.com>
2022-12-27 01:24:28 -0300

    crear producto funcional completo

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -4,7 +4,7 @@ const path = require('path');
 const productsFilePath = path.join(__dirname,'../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
-const guardar = (dato)=> fs.writeFileSync(path.join(__dirname, '../data/productos.json'), JSON.stringify(dato,null, 4), 'utf-8')
+const guardar = (dato)=> fs.writeFileSync(path.join(__dirname, '../data/productsDataBase.json'), JSON.stringify(dato,null, 4), 'utf-8')
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
 const controller = {
@@ -48,7 +48,7 @@ const controller = {
 		}
 		products.push(productoNuevo)
 		guardar(products)
-		res.redirect('/products')
+		res.redirect(`/products/${productoNuevo.id}`)
 
 	},
 
@@ -64,7 +64,22 @@ const controller = {
 	},
 	// Update - Method to update
 	update: (req, res) => {
-		return res.send("Producto a actualizar")
+		/* return res.send(req.body) */	/* Checkear los datos del [{}] */
+		idParams = +req.params.id
+		let {name,price,discount,category,description} = req.body
+
+		products.forEach(producto => {
+			if (producto.id === idParams) {
+				producto.name = name,
+				producto.price = price,
+				producto.discount = discount,
+				producto.category = category,
+				producto.description = description
+			}
+		});
+
+		guardar(products)
+		return res.redirect(`/products/${idParams.id}`)
 	},
 
 	// Delete - Delete one product from DB
